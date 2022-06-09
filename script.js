08df4550c20c29f6fe8e528562f90e8522c87a12
pritamhate <pritam.hate@outlook.com>
2022-06-10 01:31:36 +0530

    Movie seats select , movie select, ticket price and total logic implemented

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
+
+let ticketPrice = +movieSelect.value;
+
+//Update Total and Const
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+  //console.log(selectedSeatsCount);
+}
+
+//Movie Select Event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = e.target.value;
+  updateSelectedCount();
+});
+
+//Seat click event
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
