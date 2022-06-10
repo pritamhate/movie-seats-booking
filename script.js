cfaee926da83877876b01d4a5d29032643d7f444
pritamhate <pritam.hate@outlook.com>
2022-06-10 16:21:44 +0530

    Stored selected movie, seats and movie price in local storage

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
+// Save Selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 //Update Total and Const
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
@@ -19,6 +30,8 @@ function updateSelectedCount() {
 //Movie Select Event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = e.target.value;
+  //setMovieData(e.target.selectedIndex, e.target.value);
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
